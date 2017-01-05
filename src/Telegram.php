97beca3a62230ada902e5f51663cf0fe2a07a97b
LEXASOFT <lexasoft83@gmail.com>
2017-01-05 12:47:31 +0600

    Cleanup

--- src/Telegram.php
+++ src/Telegram.php
@@ -2,32 +2,32 @@
 /**
  * PHPCI - Continuous Integration for PHP
  *
- * @copyright    Copyright 2014, Block 8 Limited.
- * @license      https://github.com/Block8/PHPCI/blob/master/LICENSE.md
- * @link         https://www.phptesting.org/
+ * @copyright Copyright 2014, Block 8 Limited.
+ * @license   https://github.com/Block8/PHPCI/blob/master/LICENSE.md
+ * @link      https://www.phptesting.org/
  */
 
 namespace PHPCI\Plugin;
 
 use PHPCI\Builder;
-use PHPCI\Helper\Lang;
 use PHPCI\Model\Build;
 use b8\HttpClient;
 
 /**
  * Telegram Plugin
- * @author       LEXASOFT <lexasoft83@gmail.com>
- * @package      PHPCI
- * @subpackage   Plugins
+ * @author     LEXASOFT <lexasoft83@gmail.com>
+ * @package    PHPCI
+ * @subpackage Plugins
  */
 class Telegram implements \PHPCI\Plugin
 {
     protected $phpci;
     protected $build;
-    protected $api_key;
+    protected $apiKey;
     protected $message;
+    protected $buildMsg;
     protected $recipients;
-    protected $send_log;
+    protected $sendLog;
 
     /**
      * Standard Constructor
@@ -35,31 +35,39 @@ class Telegram implements \PHPCI\Plugin
      * @param Builder $phpci
      * @param Build   $build
      * @param array   $options
+     * @throws \Exception
      */
-    public function __construct(Builder $phpci, Build $build, array $options = array())
+    public function __construct(Builder $phpci, Build $build, array $options = [])
     {
         $this->phpci = $phpci;
         $this->build = $build;
+
         if (empty($options['api_key'])) {
             throw new \Exception("Not setting telegram api_key");
         }
+
         if (empty($options['recipients'])) {
             throw new \Exception("Not setting recipients");
         }
-        $this->api_key = $options['api_key'];
-        $this->message = '[%ICON_BUILD%] [%PROJECT_TITLE%](%PROJECT_URI%) - [Build #%BUILD%](%BUILD_URI%) has finished ' .
+
+        $this->apiKey = $options['api_key'];
+        $this->message = '[%ICON_BUILD%] [%PROJECT_TITLE%](%PROJECT_URI%)' .
+            ' - [Build #%BUILD%](%BUILD_URI%) has finished ' .
             'for commit [%SHORT_COMMIT% (%COMMIT_EMAIL%)](%COMMIT_URI%) ' .
             'on branch [%BRANCH%](%BRANCH_URI%)';
+        
         if (isset($options['message'])) {
             $this->message = $options['message'];
         }
-        $this->recipients = array();
+
+        $this->recipients = [];
         if (is_string($options['recipients'])) {
-            $this->recipients = array($options['recipients']);
+            $this->recipients = [$options['recipients']];
         } elseif (is_array($options['recipients'])) {
             $this->recipients = $options['recipients'];
         }
-        $this->send_log = isset($options['send_log']) && ((bool) $options['send_log'] !== false);
+        
+        $this->sendLog = isset($options['send_log']) && ((bool) $options['send_log'] !== false);
     }
 
     /**
@@ -68,47 +76,61 @@ class Telegram implements \PHPCI\Plugin
      */
     public function execute()
     {
-        $build_icon = $this->build->isSuccessful() ? '✅' : '❎';
-        $buildMsg = $this->build->getLog();
-        $buildMsg = str_replace(array('[0;32m', '[0;31m', '[0m', '/[0m'), array('', '', ''), $buildMsg);
-        $buildmessages = explode('RUNNING PLUGIN: ', $buildMsg);
-        $buildMsg = '';
-        foreach ($buildmessages as $bm) {
-            $pos = mb_strpos($bm, "\n");
-            $firstRow = mb_substr($bm, 0, $pos);
-            //skip long outputs
-            if (($firstRow == 'slack_notify')||($firstRow == 'php_loc')||($firstRow == 'telegram')) {
-                continue;
-            }
-            $buildMsg .= '*RUNNING PLUGIN: ' . $firstRow . "*\n";
-            $buildMsg .= $firstRow == 'composer' ? '' : ('```' . mb_substr($bm, $pos) . '```');
-        }
-        $message = $this->phpci->interpolate(str_replace(array('%ICON_BUILD%'), array($build_icon), $this->message));
+        
+        $message = $this->buildMessage();
 
         $http = new HttpClient('https://api.telegram.org');
-        $http->setHeaders(array('Content-Type: application/json'));
-        $uri = '/bot'. $this->api_key . '/sendMessage';
+        $http->setHeaders(['Content-Type: application/json']);
+        $uri = '/bot'. $this->apiKey . '/sendMessage';
 
-        foreach ($this->recipients as $chat_id) {
-            $params = array(
-                'chat_id' => $chat_id,
+        foreach ($this->recipients as $chatId) {
+            $params = [
+                'chat_id' => $chatId,
                 'text' => $message,
                 'parse_mode' => 'Markdown',
-            );
+            ];
 
             $http->post($uri, json_encode($params));
 
-            if ($this->send_log) {
-                $params = array(
-                    'chat_id' => $chat_id,
-                    'text' => $buildMsg,
+            if ($this->sendLog) {
+                $params = [
+                    'chat_id' => $chatId,
+                    'text' => $this->buildMsg,
                     'parse_mode' => 'Markdown',
-                );
+                ];
 
                 $http->post($uri, json_encode($params));
             }
         }
 
         return true;
+    }
+
+    /**
+     * Build message.
+     * @return string
+     */
+    private function buildMessage()
+    {
+        $this->buildMsg = '';
+        $buildIcon = $this->build->isSuccessful() ? '✅' : '❎';
+        $buildLog = $this->build->getLog();
+        $buildLog = str_replace(['[0;32m', '[0;31m', '[0m', '/[0m'], '', $buildLog);
+        $buildmessages = explode('RUNNING PLUGIN: ', $buildLog);
+
+        foreach ($buildmessages as $bm) {
+            $pos = mb_strpos($bm, "\n");
+            $firstRow = mb_substr($bm, 0, $pos);
+
+            //skip long outputs
+            if (in_array($firstRow, ['slack_notify', 'php_loc', 'telegram'])) {
+                continue;
+            }
+
+            $this->buildMsg .= '*RUNNING PLUGIN: ' . $firstRow . "*\n";
+            $this->buildMsg .= $firstRow == 'composer' ? '' : ('```' . mb_substr($bm, $pos) . '```');
+        }
+
+        return $this->phpci->interpolate(str_replace(['%ICON_BUILD%'], [$buildIcon], $this->message));
     }
 }
